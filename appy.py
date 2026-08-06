ead8457c3ea695b83410e174d7168f59dc820db1
jaxtraw33 <bshaw32@cox.net>
2026-08-06 05:08:53 -0700

    Update appy.py

--- appy.py
+++ appy.py
@@ -4,7 +4,6 @@ from groq import Groq
 st.set_page_config(page_title="Executive Report Cleaner", layout="wide")
 
 st.title("📝 Lead to Executive Report Cleaner AI AGENT beta")
-st.caption("24/7 Cloud Service powered by Groq Llama 3")
 st.caption("created by Brett Shaw")
 
 SYSTEM_PROMPT = """You are an executive communications editor.
